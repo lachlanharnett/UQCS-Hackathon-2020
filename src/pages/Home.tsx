73c9823c1c89ef3bb4738951331ece5c68a41e68
Unterarzt <nubytube@gmail.com>
2020-10-17 13:46:26 +1000

    Update Home.tsx to pull govt. data

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,8 @@ import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonPage, I
 import React, { useState } from 'react';
 import './Home.css';
 import '../components/GmapsAPI';
-import logo from '../images/logo.svg';
+import L from 'leaflet';
+import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
 
 //import {HTTP} from '@ionic-native/http';
 import '@ionic/react/css/padding.css';
@@ -16,25 +17,69 @@ import axios from 'axios';
 
 const Home: React.FC = () => {
 
-  
-
   const [location, setLocation] = useState('');
-  //const toiletData = HTTP.get(`https://data.gov.au/data/api/3/action/datastore_search?resource_id=100da45f-6d1d-40ad-8c47-5a0481f1fbf9&q=`, location, null);
-
-
 
   //To query, go website&q='suburb' 
   //WE MUST MAKE THEM PUT IN A SUBURB FOR BASIC DEMONSTRATION PURPOSES BECAUSE WE DON'T HAVE TIME TO MAKE OTHER OPTIONS WORK.
   const toiletGovernmentSite = "https://data.gov.au/data/api/3/action/datastore_search?resource_id=100da45f-6d1d-40ad-8c47-5a0481f1fbf9&q=";
 
+  /**
+   * 
+   * @param length The largest entry number in the function. Cannot be negative
+   */
+  function createArray(length: number) {
+    if (length < 0) { return null; }
+    var arr = new Array(length);
+    //Rows are Latitude, Columns are Longitude.
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] = new Array(2);
+    }
+    return arr;
+}
+
+
+
+  /**
+   * THIS GRABS THE DATA FROM THE GOVERNMENT DATABASE. 
+   * REQUIRES YOU TO PUSH 'ALLOW ACCESS TO LOCATION' BUTTON!!!
+   */
   function retrieveToiletData() {
     const parsingString = `${toiletGovernmentSite}${location}`;
     console.log(parsingString);
 
+    var JSONDATA;
+
     //Get the JSON objects
-    const output = axios.get(parsingString);
+    //THIS IS CORRECTLY PULLING THE GOVERNMENT DATASHEETS.
+    const output = axios.get(parsingString)
+      .then (function(response) {
+        console.log(response);
+        JSONDATA = response;
+        //console.log(output);
+        console.log("_____________________________________")
+
+        //Successful
+        console.log(JSONDATA?.data.result.records[1]);
+        console.log(JSONDATA?.data.result.records[1].Latitude);
+        
+
+        //initialise the 2D array here
+        var LatLongArray = createArray(JSONDATA?.data.result.total);
+        console.log(LatLongArray);
+
+        //Now that we have the array
+        
+
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+
+      
+
+    //console.log(output);
+
     
-    console.log(output);
   }
   
   function addToiletData() {
@@ -45,7 +90,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar class="header">
-          <img src={ logo } className="logo"></img>
+          <IonTitle class="ion-text-center" size="large">BUSTING</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent class="background">
